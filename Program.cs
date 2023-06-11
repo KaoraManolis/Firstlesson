dfe775dfe9fc66adc7f9df1d76d3b691e6b2c1c5
Kaora Manolis <kaoramanoliss@gmail.com>
2023-06-11 16:00:47 +0300

    игрушка

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿int number = new Random().Next(1, 100);
+Console.WriteLine("Я загадала число:) Угадай его:)");
+int answer = 0;
+
+while (answer != number)
+{
+    answer = Convert.ToInt32(Console.ReadLine());
+    if (answer > number)
+    {
+        Console.WriteLine("Число меньше");
+    }
+    else
+    {
+        Console.WriteLine("Число больше");
+
+    }
+}
+
+Console.WriteLine("А ты угадал!!!:) Молодец!");
